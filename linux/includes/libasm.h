bae0ab17d059d8dd36634593a264a38ea12cfc02
Le Corre <jle-corr@student.42.fr>
2020-10-26 18:56:23 +0100

    __errno_location instead of ___error, in linux, to find global errno address

--- linux/includes/libasm.h
+++ linux/includes/libasm.h
@@ -11,8 +11,8 @@
 size_t		ft_strlen(const char *str) asm("ft_strlen");
 char		*ft_strcpy(char *dst,const char *src) asm("ft_strcpy");
 int		ft_strcmp(const char *s1,const  char *s2) asm("ft_strcmp");
-ssize_t		ft_write(int fildes, const void *buf, size_t nbyte) asm("ft_write");
 ssize_t		ft_read(int fildes, void *buf, size_t nbyte) asm("ft_read");
+ssize_t		ft_write(int fildes, const void *buf, size_t nbyte) asm("ft_write");
 char		*ft_strdup(const char *s1) asm("ft_strdup");
 
 #endif
--- linux/src/ft_read.s
+++ linux/src/ft_read.s
@@ -1,5 +1,5 @@
 global ft_read
-extern ___error
+extern __errno_location
 default rel
 
 section .text
@@ -7,11 +7,13 @@ section .text
 ft_read:
 	mov	rax, 0			;read syscall linux ID
 	syscall
-	jc	error			;syscall error sets CARRY FLAG (CF) in RFLAGS register
-	ret					;to 1. JC instruction check CF. If JC is true we jump to
-error:					;error code. Syscall returns errnb into RAX register,but
+	cmp	rax, 0
+	jl	errcode			;syscall error sets the negative value of error number in rax
+	ret					;If rax is negative we jump to
+errcode:				;error code. Syscall returns -errnb into RAX register,but
+	neg rax
 	mov r10, rax		;read has to return (-1). If an error occurs, we have
-	call ___error		;to put error number into extern ERRNO variable then put
+	call __errno_location	;to put error number into extern ERRNO variable then put
 	mov [rax], r10		;return value into RAX. ___error call give us in rax the
 	mov rax, -1			;ERRNO variable address so that we can put syscall error
 	ret					;return into, then set read return to (-1);
